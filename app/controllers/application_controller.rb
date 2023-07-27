9d50d993e12603bbbf8ef510b48fd276d78901ab
shirose50413 <132430261+shirose50413@users.noreply.github.com>
2023-07-27 23:10:07 +0000

    [Finish Nagano-CAKE]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,23 +3,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def after_sign_in_path_for(resource_or_scope)
-      if resource_or_scope.is_a?(Admin)
-          admin_root_path
-      else
-          customers_path
-      end
-  end
-
   def
   after_sign_out_path_for(resource_or_scope)
-      if resource_or_scope == :user
-          root_path
-      elsif resource_or_scope == :admin
-          new_admin_session_path
-      else
-          root_path
-      end
+    if resource_or_scope == :customer
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   protected
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,12 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_customer, only: [:create]
+  # before_action :reject_customer, only: [:create]
+
+  def after_sign_in_path_for(_resource)
+    root_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
